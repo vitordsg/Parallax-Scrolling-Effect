bfec93afaa73321d8810723eed024f3d78ed38f5
Vitor Santos <vitinho6.6@hotmail.com>
2023-08-30 02:04:12 -0300

    Testes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const content = document.querySelector(".content");
 const section = document.querySelector("section");
 const image_container = document.querySelector(".imgContainer");
 const opacity = document.querySelectorAll(".opacity");
-const border = document.querySelector(".border");
+const border = document.querySelectorAll(".border");
 
 let header_height = header.offsetHeight;
 let section_height = section.offsetHeight;
@@ -27,8 +27,17 @@ window.addEventListener('scroll', () => {
     big_title.style.opacity = - scroll / (header_height / 2) + 1;
     shadow.style.height = `${scroll * 0.5 + 300}px`;
 
-    content.style.transform = `translateY(${scroll / (section_height + sectionY.top) * 50 - 50}px)`;
-    image_container.style.transform = `translateY(${scroll / (section_height + sectionY.top) * -50 + 50}px)`;
+    // content.style.transform = `translateY(${scroll / (section_height + sectionY.top) * 50 - 50}px)`;
+    // image_container.style.transform = `translateY(${scroll / (section_height + sectionY.top) * -50 + 50}px)`;
 
-    border.style.width = `${scroll / (sectionY.top + section_height) * 30}%`;
+    let scrollSection = sectionY.top
+    border.forEach(element => {
+        if (scrollSection < -200) {
+            element.style.width = `${scroll / (scrollSection + section_height) * 30}%`;
+        } 
+        else {
+            element.style.width = `${scroll / (scrollSection + section_height) * 30}%`;
+        }
+        console.log(scrollSection)
+    })
 })
